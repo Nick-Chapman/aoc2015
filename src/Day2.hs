ef3907bccffc6a7e91331174893e16f137800e82
Nick Chapman <nic@amadido.co.uk>
2025-09-24 11:23:07 +0100

    day2

--- src/Day2.hs
+++ src/Day2.hs
@@ -9,6 +9,8 @@ main = do
   inp <- parse gram <$> readFile "input/day2.input"
   print ("day2, part1 (sample)", check [58,43] $ part1 sam)
   print ("day2, part1", check 1598415 $ sum $ part1 inp)
+  print ("day2, part2 (sample)", check [34,14] $ part2 sam)
+  print ("day2, part2", check 3812909 $ sum $ part2 inp)
 
 type Box = (Int,Int,Int)
 
@@ -31,3 +33,9 @@ part1 = map paper
       let ac = a*c
       let bc = b*c
       2*(ab+ac+bc) + minimum [ab,ac,bc]
+
+part2 :: [Box] -> [Int]
+part2 = map paper
+  where
+    paper (a,b,c) = do
+      2 * minimum [a+b,a+c,b+c] + a*b*c
