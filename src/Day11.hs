97fa24f57c8769c21aaf3d5b88f661b6584d18ce
Nick Chapman <nic@amadido.co.uk>
2025-09-30 09:38:25 +0100

    day 11

---
+++ src/Day11.hs
@@ -0,0 +1,8 @@
+module Day11 (main) where
+
+main :: IO ()
+main = do
+  let _inp = "cqjxjnds"
+  -- no code required; hand solve is trivial
+  print ("day11, part1", "cqjxxyzz")
+  print ("day11, part2", "cqkaabcc")
